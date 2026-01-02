e2ab6b43f32c8b40615fd0c9a36889d03608882c
jianlew04 <jianlew04@gmail.com>
2026-01-03 00:53:55 +0800

    Update DataPrep.cpp

--- DataPrep.cpp
+++ DataPrep.cpp
@@ -1,9 +1,11 @@
 #include <fstream>//don't change this line
 #include <string>
+#include <iostream>
 #include "header/DataPrep.hpp"//include any other necessary headers like iostream,cmath ect above this line
 using namespace std;
 //include any other necessary headers like iostream,cmath ect.
 void DataPrep(string Material)//don't change this line
 {
-//Paste or type code here
+//Paste or type code here 
+cout<<"siu";
 }
