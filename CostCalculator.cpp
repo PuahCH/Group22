2d3d29bca023ba232004adbb0c0cf0462237c889
homanpee <mbream18@gmail.com>
2026-01-02 13:39:11 +0800

    Create CostCalculator.cpp

---
+++ CostCalculator.cpp
@@ -0,0 +1,6 @@
+#include "header/CostCalculator.hpp" //don't change this line
+//include any other necessary headers like iostream,cmath ect.
+void CostCalculator()//don't change this line
+{
+//Paste or type code here
+}
