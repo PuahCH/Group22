c4d2f404718c80ba3c96dc60fe89894e67630778
PuahCH <mbream18@gmail.com>
2026-01-15 12:05:16 +0800

    Update CostCalculator.cpp

--- CostCalculator.cpp
+++ CostCalculator.cpp
@@ -1,4 +1,4 @@
-#include <fstream>//don't change this line
+#include <ifstream>//don't change this line
 #include "header/CostCalculator.hpp"//include any other necessary headers like iostream,cmath ect above this line
 void CostCalculator()//don't change this line
 {
